311945ddcd4409e2744d473c44b68e4ac06b2d28
Холоднов Эмиль <emil.kholod@gmail.com>
2025-05-06 12:50:14 +0500

    CHG: Fix error and increase version

--- flake8_diff_only/checker.py
+++ flake8_diff_only/checker.py
@@ -3,10 +3,10 @@ import ast
 
 class Flake8DiffOnlyChecker:
     name = "flake8-diff-only"
-    version = "0.1.4"
+    version = "0.1.5"
 
     def __init__(self, tree: ast.AST, filename: str):
         pass
 
     def run(self):  # type: ignore[no-untyped-def]
-        pass
+        return []
