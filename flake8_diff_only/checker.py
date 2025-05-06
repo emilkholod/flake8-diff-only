39f5406f9e917f9797a8f31ffaacc7b9a6dc1432
Холоднов Эмиль <emil.kholod@gmail.com>
2025-05-06 12:46:50 +0500

    CHG: Update version

--- flake8_diff_only/checker.py
+++ flake8_diff_only/checker.py
@@ -3,7 +3,7 @@ import ast
 
 class Flake8DiffOnlyChecker:
     name = "flake8-diff-only"
-    version = "0.1.3"
+    version = "0.1.4"
 
     def __init__(self, tree: ast.AST, filename: str):
         pass
